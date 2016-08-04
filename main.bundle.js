925a7bb967c126eb216e595eb3e5b856ad4c6fda
Jeff-Duke <Jeff.Duke@Gmail.com>
2016-08-04 10:39:44 -0600

    Update main.bundle.js

--- main.bundle.js
+++ main.bundle.js
@@ -77,7 +77,7 @@
 	}
 
 	function drawBall() {
-	  almond.src = '../img/little-almond.jpg';
+	  almond.src = 'Game-Time/img/little-almond.PNG';
 	  context.drawImage(almond, world.ball.x, world.ball.y, world.ball.height, world.ball.width);
 	}
 
